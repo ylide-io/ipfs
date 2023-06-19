35e254d8da9b59aec3ffdea6194dc2f7f65bd0e3
Danila Simonov <ek@ek-mail.ru>
2023-06-19 20:26:14 +0200

    stickers cache

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,36 @@ import http from 'http';
 import busboy from 'busboy';
 import fetch from 'node-fetch';
 import FormData from 'form-data';
+import fs from 'fs';
+
+const cachedHashes: string[] = [
+	'QmPNxHsuCpjCoUx8hvanCdzgwQH5CcXcUusvJ2cLyYz8r4',
+	'QmUvZc5jmPa5GK7R5KATH5i4n9uUQPh8vPC9k7oB9oMdER',
+	'QmdyTMTvKZLg33aRy3Y4ELDu5mn7pvcFbLL8KmSxhM96YR',
+	'QmWEBrZzyak2qbDu825q385arxURqKM7GiyffMBTxzidct',
+	'QmTRYuhzMYZ1vF8nFNh41K2KN6pF7VQu25BEmGbPoj6Ny8',
+	'QmVhfBAsFKnoV4dFtaWWqEAtNTnQmAW3TjJogakvj9VwuU',
+	'QmWVkC5on7f6QD4gJTRfjNb6GVYdPa6YJn8JGuU5kDdpZX',
+	'QmXnUQwtiTbmedQwZ74LybbHJWMgePTvoRPTUTgCEVkWvT',
+	'QmQf26iMvegpgNAiuc2SqDVL2vvwL8Q8wHJajVz3ZzWpZ3',
+	'QmQ2cdJmHPtwQEKysejQLfc3JCiwYd7X7CuqLtgHUqNH7U',
+	'QmRQk35pqdBj8xo57RzphdpiK5hmf7kGHZ45V2uLAi34UG',
+	'QmTovX2GyBwJ5uKKudQ6r5zwjbKr8DEibp7Emodqw19Ta2',
+	'QmU9CZfWpAbRNfpU2m515t5mPCiEs3TRH7G1rNdbyK9ZRg',
+	'QmU8E9wHZq4TzCAzg4CL9EC7CGD2gBKdH6PbAMwGo2ZqUo',
+	'QmRC1MLd2PEb3dRTKNE5z57KPgEfTkvAPX5tfvHjAZiScb',
+	'Qme7Cuyk5NezKTXu5gCoNxAXrGv9T6JaUeBGTd1wmPbfpo',
+	'QmetxtnQQP71YEn16YkwbrWhN98hjewgSq5vQKQjPpxeE1',
+	'QmPrTT1x37dDwDsCPzvAppLgxQhsAExLyr8RjwYyRSE2oV',
+	'QmdfDTHJGqFGkds6FaS4NTEqVPUapp33CHChk8Nj5ToCTz',
+	'QmTAGJ7HEEFtmqPaUe3bJZwZ8RUKwBDug94AKG5rJ28puT',
+	'QmdguqNpb2bUKg6znVSyXtKRaNrtrYVxchaof7xryjarhE',
+	'QmW4oWhz6onHJSaWYBTVF68XCgvr8MSrs6m73dJd3tRLTb',
+	'QmX9QLWtfw1Q2RddP6UNEzRFSkZwy7NcNbwxstqRpBg5P2',
+	'QmZVpzzAyKnFXDLpJze7H22p8igxRfuLHCikPA3YMRhLg7',
+	'QmSwxecFnFjbe44HJ3pQdLrytrZNcM48XGPbvaTbxWZsCk',
+	'QmbYpa9m8QsCuyTG4tPYHtok2Cf7raJrtap9kMsND5Y25x',
+];
 
 const run = async () => {
 	const env = dotenv.config();
@@ -12,6 +42,27 @@ const run = async () => {
 	const IPFS_API = env.parsed!.IPFS_API;
 
 	const origins = ['http://localhost:3000', 'https://hub.ylide.io'];
+
+	if (!fs.existsSync('cache')) {
+		fs.mkdirSync('cache');
+	}
+
+	for (const hash of cachedHashes) {
+		if (!fs.existsSync(`cache/${hash}`)) {
+			const response = await fetch(`https://ipfs.infura.io:5001/api/v0/cat?arg=${hash}`, {
+				method: 'POST',
+				headers: {
+					Authorization: `Basic ${Buffer.from(`${IPFS_PUBLIC_KEY}:${IPFS_SECRET_KEY}`).toString('base64')}`,
+				},
+			});
+			if (response.status !== 200) {
+				console.error(`Error downloading cache file: ${response.status} ${response.statusText}`);
+				return;
+			}
+			const stream = fs.createWriteStream(`cache/${hash}`);
+			response.body.pipe(stream);
+		}
+	}
 
 	const PORT = parseInt(env.parsed!.PORT, 10);
 
@@ -99,6 +150,22 @@ const run = async () => {
 		} else if (req.method === 'GET') {
 			if (req.url && req.url.startsWith('/file/')) {
 				const hash = new URL(`a://b${req.url}`).pathname.split('/')[2];
+				if (cachedHashes.includes(hash)) {
+					const stream = fs.createReadStream(`cache/${hash}`);
+					res.writeHead(
+						200,
+						req.headers.origin
+							? {
+									'Access-Control-Allow-Origin': req.headers.origin,
+									'Content-Type': 'application/octet-stream',
+							  }
+							: {
+									'Content-Type': 'application/octet-stream',
+							  },
+					);
+					stream.pipe(res);
+					return;
+				}
 				fetch(`https://ipfs.infura.io:5001/api/v0/cat?arg=${hash}`, {
 					method: 'POST',
 					headers: {
